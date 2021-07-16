f46518af9f2cf9da399a7db8689c400d9cb903a3
Daniel <daniel@tohka.us>
2021-07-16 14:35:30 +0300

    version: Bump version to v1.9.1

--- FlashPatch/UpdateChecker.cs
+++ FlashPatch/UpdateChecker.cs
@@ -32,7 +32,7 @@ namespace FlashPatch {
     }
 
     public class UpdateChecker {
-        private static readonly string VERSION = "v1.9";
+        private static readonly string VERSION = "v1.9.1";
         private static readonly string AUTHOR = "darktohka";
         private static readonly string REPO = "FlashPatch";
         private static readonly string USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36";
