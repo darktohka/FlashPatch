44c5627fe809f48ae13b42a0c9f5a0804ffab06e
darktohka <daniel@tohka.us>
2024-12-21 12:46:59 +0200

    version: Bump version to v1.28

--- FlashPatch/UpdateChecker.cs
+++ FlashPatch/UpdateChecker.cs
@@ -32,7 +32,7 @@ namespace FlashPatch {
     }
 
     public class UpdateChecker {
-        private static readonly string VERSION = "v1.27.1";
+        private static readonly string VERSION = "v1.28";
         private static readonly string AUTHOR = "darktohka";
         private static readonly string REPO = "FlashPatch";
         private static readonly string USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36";
